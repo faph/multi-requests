67a0965bf98f5eb332da3f471b6d65c85642f2e1
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2022-02-20 17:42:23 +0000

    Update test_acceptance.py

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -2,3 +2,9 @@ import multi_requests
 
 def test_has_version():
     assert multi_requests.__version__
+
+def test_multiple_urls_return_responses():
+    session = multi_requests.MultiSession()
+    urls = ["", ""]
+    responses = session.get(urls)
+    assert len(responses) == 2
