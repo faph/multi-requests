1e470303d282e00311fa0507b8fca12fdd2b9b0b
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2022-02-20 20:04:00 +0000

    Update __init__.py

--- src/multi_requests/__init__.py
+++ src/multi_requests/__init__.py
@@ -5,3 +5,7 @@ multi-requests API
 import importlib.metadata
 
 __version__ = importlib.metadata.version("multi-requests")
+
+
+class MultiSession():
+    """A HTTP client session supporting simultaneous requests"""
