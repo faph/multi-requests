5f6625b206acba1f5c68d0003819c9d3f67e9596
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2022-02-21 20:22:54 +0000

    Update test_acceptance.py

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -16,14 +16,23 @@ def test_multiple_urls_return_responses(session):
     urls = ["https://catfact.ninja/facts", "https://catfact.ninja/breeds"]
     responses = session.get(urls)
     assert len(responses) == 2
+    
+def test_multiple_urls_with_single_params_return_responses(session):
+    urls = [
+        "https://catfact.ninja/facts",
+        "https://catfact.ninja/facts",
+    ]
+    params = [
+        {"max_length": 100},
+        {"limit": 2},
+    ]
+    responses = session.get(url, params=params)
+    assert len(responses) == 2
 
 
 def test_multiple_params_return_responses(session):
     url = "https://catfact.ninja/facts"
-    params = [
-        {"max_length": 100},
-        {"max_length": 200},
-    ]
+    params = {"max_length": 100}
     responses = session.get(url, params=params)
     assert len(responses) == 2
 
