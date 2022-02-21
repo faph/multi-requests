1888b88754b27e571d349b663fb6847366b6e703
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2022-02-21 20:34:40 +0000

    Update __init__.py

--- src/multi_requests/__init__.py
+++ src/multi_requests/__init__.py
@@ -3,9 +3,20 @@ multi-requests API
 """
 
 import importlib.metadata
+from typing import List, Union
+
+import requests
+
 
 __version__ = importlib.metadata.version("multi-requests")
 
 
 class MultiSession:
     """A HTTP client session supporting simultaneous requests"""
+    
+    def __init__(self):
+        self._session = requests.Session()
+        
+    def get(url: Union[str, List[str]], **kwargs) -> requests.Response:
+        """Send a HTTP GET request"""
+        return self._session.get(url, **kwargs)
