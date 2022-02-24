a598fff512fd389b6a030e8b63d0e3353643da7b
Florenz A.P. Hollebrandse <faph@users.noreply.github.com>
2022-02-24 16:18:02 +0000

    initial implementation of multi GET requests

--- src/multi_requests/__init__.py
+++ src/multi_requests/__init__.py
@@ -1,9 +1,9 @@
 """
 multi-requests API
 """
-
+import functools
 import importlib.metadata
-from typing import List, Union
+from typing import Any, List, Union
 
 import requests
 
@@ -19,7 +19,24 @@ class MultiSession:
 
     def get(self, url: Union[str, List[str]], **kwargs) -> List[requests.Response]:
         """Send a HTTP GET request"""
-        if isinstance(url, str):
-            return self._session.get(url, **kwargs)
-        else:
-            return [self._session.get(single_url, **kwargs) for single_url in url]
+        all_kwargs = {"url": url, **kwargs}
+        # Arguments that are shared and apply to all requests
+        common_kwargs = {
+            param: value for param, value in all_kwargs.items() if not _is_sequence(value)
+        }
+        # Arguments for which there is a different value for each request
+        sequence_kwargs = {
+            param: value for param, value in all_kwargs.items() if _is_sequence(value)
+        }
+        # Group them into a list, each item containing the kwargs for individual requests
+        sequence_kwargs_list = [
+            dict(zip(sequence_kwargs, group)) for group in list(zip(*sequence_kwargs.values()))
+        ]
+        request_fn = functools.partial(self._session.get, **common_kwargs)
+        responses = [request_fn(**request_kwargs) for request_kwargs in sequence_kwargs_list]
+        return responses
+
+
+def _is_sequence(value: Any) -> bool:
+    """Return whether a given parameter value is a sequence or not"""
+    return isinstance(value, list)  # TODO: improve
