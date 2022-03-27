428016a1cdea320053368f1a69862a823299a386
Florenz A.P. Hollebrandse <faph@users.noreply.github.com>
2022-03-27 21:14:54 +0100

    docs

--- src/multi_requests/__init__.py
+++ src/multi_requests/__init__.py
@@ -1,5 +1,16 @@
 """
 multi-requests API
+
+Usage
+-----
+
+>>> import multi_requests
+>>> session = multi_requests.MultiSession()
+>>> session.get([
+...     "https://api.github.com/users/defunkt",
+...     "https://api.github.com/users/faph",
+... ])
+[<Response [200]>, <Response [200]>]
 """
 import importlib.metadata
 from typing import List, Union
@@ -25,6 +36,12 @@ class MultiSession:
         value in such a list would correspond to a single request. If multiple (keyword) arguments
         are lists they must have the same length.
         """
+        return self.request("get", url=url, **kwargs)
+
+    def request(self, method: str, url: Union[str, List[str]], **kwargs) -> List[requests.Response]:
+        """
+        Send a HTTP request
+        """
         all_kwargs = {"url": url, **kwargs}
         # Broadcast scalar kwarg values to same length as list kwargs
         kwarg_values_sequence = more_itertools.zip_broadcast(
@@ -34,5 +51,5 @@ class MultiSession:
         )
         # Combine with the kwarg keys
         kwarg_dicts_sequence = (dict(zip(all_kwargs, values)) for values in kwarg_values_sequence)
-        responses = [self._session.get(**kwarg_dict) for kwarg_dict in kwarg_dicts_sequence]
+        responses = [self._session.request(method, **kwarg_dict) for kwarg_dict in kwarg_dicts_sequence]
         return responses
